3070372af643a196d2f491ac29aaba099809eefd
Min Guo <31596377+eguomin@users.noreply.github.com>
2024-10-19 16:45:16 +0800

    update

---
+++ imDecorr_batch_Chad_Actin.m
@@ -0,0 +1,158 @@
+% % set path and load some data
+addpath('funcs')
+clear all;
+filePath = 'D:\Project_multiStepDL\AO_data_FromChad\Cell_actin\Figs\01_slice\';
+fileOut = [filePath, 'FRC_Statistics'];
+
+pixelSize = 108; % nm
+pps = 5; % projected pixel size of 15nm
+% typical parameters for resolution estimate
+Nr = 50;
+Ng = 10;
+r = linspace(0,1,Nr);
+GPU = 1;
+apSize =20;
+tStart = tic;
+
+folderIn = 'original\';
+fileFolderIn = [filePath, folderIn];
+files = dir(fullfile(fileFolderIn, '*.tif'));
+imgNumStart = 1;
+imgNumEnd = length(files);
+dNum = (imgNumEnd - imgNumStart + 1);
+resValues = zeros(4, dNum);
+for i = imgNumStart:imgNumEnd
+    cTime1 = toc(tStart);
+    disp(['Image i #: ', num2str(i)]);
+    fileIn = [fileFolderIn, files(i).name];
+    img0 = double(ReadTifStack(fileIn));
+    imSize0 = size(img0);
+    exSize = [40, 40];
+    img = img0(exSize(1)+1:imSize0(1)-exSize(1), exSize(2)+1:imSize0(2)-exSize(2));
+    % % apodize image edges with a cosine function
+    imSlice = apodImRect(img, apSize);
+    % % compute resolution
+    figID = 100;
+    if GPU
+        g = gpuDevice(1);
+        [kcMax,A0] = getDcorr(gpuArray(imSlice),r,Ng,figID);
+    else
+        [kcMax,A0] = getDcorr(imSlice,r,Ng,figID);
+    end
+    disp(['   kcMax : ',num2str(kcMax,3),', A0 : ',num2str(A0,3)])
+    close all;
+    ress = pixelSize * 2/kcMax;
+    resValues(1, i) =ress;
+    cTime2 = toc(tStart);
+    disp(['... time cost: ', num2str(cTime2-cTime1)]);
+end
+
+folderIn = 'aberrated\';
+fileFolderIn = [filePath, folderIn];
+files = dir(fullfile(fileFolderIn, '*.tif'));
+imgNumStart = 1;
+imgNumEnd = length(files);
+dNum = (imgNumEnd - imgNumStart + 1);
+% resValues = zeros(4, dNum);
+for i = imgNumStart:imgNumEnd
+    cTime1 = toc(tStart);
+    disp(['Image i #: ', num2str(i)]);
+    fileIn = [fileFolderIn, files(i).name];
+    img0 = double(ReadTifStack(fileIn));
+    imSize0 = size(img0);
+    exSize = [40, 40];
+    img = img0(exSize(1)+1:imSize0(1)-exSize(1), exSize(2)+1:imSize0(2)-exSize(2));
+    % % apodize image edges with a cosine function
+    imSlice = apodImRect(img, apSize);
+    % % compute resolution
+    figID = 100;
+    if GPU
+        g = gpuDevice(1);
+        [kcMax,A0] = getDcorr(gpuArray(imSlice),r,Ng,figID);
+    else
+        [kcMax,A0] = getDcorr(imSlice,r,Ng,figID);
+    end
+    disp(['   kcMax : ',num2str(kcMax,3),', A0 : ',num2str(A0,3)])
+    close all;
+    ress = pixelSize * 2/kcMax;
+    resValues(2, i) =ress;
+    cTime2 = toc(tStart);
+    disp(['... time cost: ', num2str(cTime2-cTime1)]);
+end
+
+folderIn = 'AO\';
+fileFolderIn = [filePath, folderIn];
+files = dir(fullfile(fileFolderIn, '*.tif'));
+imgNumStart = 1;
+imgNumEnd = length(files);
+dNum = (imgNumEnd - imgNumStart + 1);
+% resValues = zeros(4, dNum);
+for i = imgNumStart:imgNumEnd
+    cTime1 = toc(tStart);
+    disp(['Image i #: ', num2str(i)]);
+    fileIn = [fileFolderIn, files(i).name];
+    img0 = double(ReadTifStack(fileIn));
+    imSize0 = size(img0);
+    exSize = [40, 40];
+    img = img0(exSize(1)+1:imSize0(1)-exSize(1), exSize(2)+1:imSize0(2)-exSize(2));
+    % % apodize image edges with a cosine function
+    imSlice = apodImRect(img, apSize);
+    % % compute resolution
+    figID = 100;
+    if GPU
+        g = gpuDevice(1);
+        [kcMax,A0] = getDcorr(gpuArray(imSlice),r,Ng,figID);
+    else
+        [kcMax,A0] = getDcorr(imSlice,r,Ng,figID);
+    end
+    disp(['   kcMax : ',num2str(kcMax,3),', A0 : ',num2str(A0,3)])
+    close all;
+    ress = pixelSize * 2/kcMax;
+    resValues(3, i) =ress;
+    cTime2 = toc(tStart);
+    disp(['... time cost: ', num2str(cTime2-cTime1)]);
+end
+
+folderIn = 'DeAbe\';
+fileFolderIn = [filePath, folderIn];
+files = dir(fullfile(fileFolderIn, '*.tif'));
+imgNumStart = 1;
+imgNumEnd = length(files);
+dNum = (imgNumEnd - imgNumStart + 1);
+% resValues = zeros(4, dNum);
+for i = imgNumStart:imgNumEnd
+    cTime1 = toc(tStart);
+    disp(['Image i #: ', num2str(i)]);
+    fileIn = [fileFolderIn, files(i).name];
+    img0 = double(ReadTifStack(fileIn));
+    imSize0 = size(img0);
+    exSize = [40, 40];
+    img = img0(exSize(1)+1:imSize0(1)-exSize(1), exSize(2)+1:imSize0(2)-exSize(2));
+    % % apodize image edges with a cosine function
+    imSlice = apodImRect(img, apSize);
+    % % compute resolution
+    figID = 100;
+    if GPU
+        g = gpuDevice(1);
+        [kcMax,A0] = getDcorr(gpuArray(imSlice),r,Ng,figID);
+    else
+        [kcMax,A0] = getDcorr(imSlice,r,Ng,figID);
+    end
+    disp(['   kcMax : ',num2str(kcMax,3),', A0 : ',num2str(A0,3)])
+    close all;
+    ress = pixelSize * 2/kcMax;
+    resValues(4, i) =ress;
+    cTime2 = toc(tStart);
+    disp(['... time cost: ', num2str(cTime2-cTime1)]);
+end
+
+
+resMean = mean(resValues,2);
+resSD = std(resValues',1)';
+clear img0 img
+csvwrite([fileOut, '.csv'], resValues);
+csvwrite([fileOut, '_mean_SD.csv'], [resMean resSD]);
+save([fileOut, '.mat']);
+cTime = toc(tStart);
+disp(['Processing completed!!! Total time cost:', num2str(cTime), ' s']);
+
--- imDecorr_stack_batch.m
+++ imDecorr_stack_batch.m
@@ -1,8 +1,8 @@
 % % set path and load some data
 addpath('funcs')
 clear all;
-fileFolderIn = 'D:\Project_multiStepDL\AO_data_FromChad\Zebrafish\depth_DeAbe\DeAbe_RCAN\';
-fileOut = 'D:\Project_multiStepDL\AO_data_FromChad\Zebrafish\depth_DeAbe\FRC_DeAbe_RCAN';
+fileFolderIn = 'D:\Project_multiStepDL\AO_data_FromChad\Zebrafish\depth\DeAbe\DeAbe_BasicVSR\';
+fileOut = 'D:\Project_multiStepDL\AO_data_FromChad\Zebrafish\depth\DeAbe\FRC_DeAbe_BasicVSR';
 fileNameBase = 'scan_';
 imgNumStart = 1;
 imgNumEnd = 15;
@@ -66,8 +66,9 @@ save([fileOut, '.mat']);
 cTime = toc(tStart);
 disp(['Processing completed!!! Total time cost:', num2str(cTime), ' s']);
 
-fileFolderIn = 'D:\Project_multiStepDL\AO_data_FromChad\Zebrafish\depth_raw\noAO\';
-fileOut = 'D:\Project_multiStepDL\AO_data_FromChad\Zebrafish\depth_DeAbe\FRC_noAO';
+clear all;
+fileFolderIn = 'D:\Project_multiStepDL\AO_data_FromChad\Zebrafish\depth\DeAbe\DeAbe_CARE\';
+fileOut = 'D:\Project_multiStepDL\AO_data_FromChad\Zebrafish\depth\DeAbe\FRC_DeAbe_CARE';
 fileNameBase = 'scan_';
 imgNumStart = 1;
 imgNumEnd = 15;
@@ -130,3 +131,200 @@ csvwrite([fileOut, '_mean_SD.csv'], [resMean resSD]);
 save([fileOut, '.mat']);
 cTime = toc(tStart);
 disp(['Processing completed!!! Total time cost:', num2str(cTime), ' s']);
+
+clear all;
+fileFolderIn = 'D:\Project_multiStepDL\AO_data_FromChad\Zebrafish\depth\DeAbe\Denoise_N2V\';
+fileOut = 'D:\Project_multiStepDL\AO_data_FromChad\Zebrafish\depth\DeAbe\FRC_Denoise_N2V';
+fileNameBase = 'scan_';
+imgNumStart = 1;
+imgNumEnd = 15;
+% fileIn = 'I:\AO_Project\DeepLearning\TwoPhotonChris\DL_results\Tom20_Heart\DL_DeAbe\img_6.tif';
+fileIn = [fileFolderIn, fileNameBase, num2str(imgNumStart), '.tif'];
+img0 = double(ReadTifStack(fileIn));
+pixelSize = 108; % nm
+
+imSize0 = size(img0);
+exSize = [10, 10, 10];
+imSize = imSize0 - exSize * 2;
+
+pps = 5; % projected pixel size of 15nm
+% typical parameters for resolution estimate
+Nr = 50;
+Ng = 10;
+r = linspace(0,1,Nr);
+GPU = 1;
+
+Sz = imSize(3);
+kcMaxs = zeros(1, Sz);
+apSize =20;
+
+dNum = (imgNumEnd - imgNumStart + 1);
+resValues = zeros(dNum, Sz);
+tStart = tic;
+for i = imgNumStart:imgNumEnd
+    cTime1 = toc(tStart);
+    disp(['Image i #: ', num2str(i)]);
+    fileImgSample = [fileFolderIn, fileNameBase, num2str(i), '.tif'];
+    img0 = double(ReadTifStack(fileImgSample));
+    img = alignsize3d(img0, imSize);
+    for j = 1:Sz
+        % % apodize image edges with a cosine function
+        imSlice = apodImRect(img(:,:,j), apSize);
+        % % compute resolution
+
+        figID = 100;
+        if GPU
+            g = gpuDevice(1);
+            [kcMax,A0] = getDcorr(gpuArray(imSlice),r,Ng,figID);
+        else
+            [kcMax,A0] = getDcorr(imSlice,r,Ng,figID);
+        end
+        disp(['   kcMax : ',num2str(kcMax,3),', A0 : ',num2str(A0,3)])
+        kcMaxs(j) = kcMax;
+        close all;
+    end
+    ress = pixelSize * 2./kcMaxs;
+
+    resValues(i, :) =ress;
+    cTime2 = toc(tStart);
+    disp(['... time cost: ', num2str(cTime2-cTime1)]);
+end
+resMean = mean(resValues,2);
+resSD = std(resValues',1)';
+clear img0 img
+csvwrite([fileOut, '.csv'], resValues);
+csvwrite([fileOut, '_mean_SD.csv'], [resMean resSD]);
+save([fileOut, '.mat']);
+cTime = toc(tStart);
+disp(['Processing completed!!! Total time cost:', num2str(cTime), ' s']);
+
+clear all;
+fileFolderIn = 'D:\Project_multiStepDL\AO_data_FromChad\Zebrafish\depth\DeAbe\Denoise_NLM\';
+fileOut = 'D:\Project_multiStepDL\AO_data_FromChad\Zebrafish\depth\DeAbe\FRC_Denoise_NLM';
+fileNameBase = 'scan_';
+imgNumStart = 1;
+imgNumEnd = 15;
+% fileIn = 'I:\AO_Project\DeepLearning\TwoPhotonChris\DL_results\Tom20_Heart\DL_DeAbe\img_6.tif';
+fileIn = [fileFolderIn, fileNameBase, num2str(imgNumStart), '.tif'];
+img0 = double(ReadTifStack(fileIn));
+pixelSize = 108; % nm
+
+imSize0 = size(img0);
+exSize = [10, 10, 10];
+imSize = imSize0 - exSize * 2;
+
+pps = 5; % projected pixel size of 15nm
+% typical parameters for resolution estimate
+Nr = 50;
+Ng = 10;
+r = linspace(0,1,Nr);
+GPU = 1;
+
+Sz = imSize(3);
+kcMaxs = zeros(1, Sz);
+apSize =20;
+
+dNum = (imgNumEnd - imgNumStart + 1);
+resValues = zeros(dNum, Sz);
+tStart = tic;
+for i = imgNumStart:imgNumEnd
+    cTime1 = toc(tStart);
+    disp(['Image i #: ', num2str(i)]);
+    fileImgSample = [fileFolderIn, fileNameBase, num2str(i), '.tif'];
+    img0 = double(ReadTifStack(fileImgSample));
+    img = alignsize3d(img0, imSize);
+    for j = 1:Sz
+        % % apodize image edges with a cosine function
+        imSlice = apodImRect(img(:,:,j), apSize);
+        % % compute resolution
+
+        figID = 100;
+        if GPU
+            g = gpuDevice(1);
+            [kcMax,A0] = getDcorr(gpuArray(imSlice),r,Ng,figID);
+        else
+            [kcMax,A0] = getDcorr(imSlice,r,Ng,figID);
+        end
+        disp(['   kcMax : ',num2str(kcMax,3),', A0 : ',num2str(A0,3)])
+        kcMaxs(j) = kcMax;
+        close all;
+    end
+    ress = pixelSize * 2./kcMaxs;
+
+    resValues(i, :) =ress;
+    cTime2 = toc(tStart);
+    disp(['... time cost: ', num2str(cTime2-cTime1)]);
+end
+resMean = mean(resValues,2);
+resSD = std(resValues',1)';
+clear img0 img
+csvwrite([fileOut, '.csv'], resValues);
+csvwrite([fileOut, '_mean_SD.csv'], [resMean resSD]);
+save([fileOut, '.mat']);
+cTime = toc(tStart);
+disp(['Processing completed!!! Total time cost:', num2str(cTime), ' s']);
+
+% fileFolderIn = 'D:\Project_multiStepDL\AO_data_FromChad\Zebrafish\depth\raw\noAO\';
+% fileOut = 'D:\Project_multiStepDL\AO_data_FromChad\Zebrafish\depth\DeAbe\FRC_noAO';
+% fileNameBase = 'scan_';
+% imgNumStart = 1;
+% imgNumEnd = 15;
+% % fileIn = 'I:\AO_Project\DeepLearning\TwoPhotonChris\DL_results\Tom20_Heart\DL_DeAbe\img_6.tif';
+% fileIn = [fileFolderIn, fileNameBase, num2str(imgNumStart), '.tif'];
+% img0 = double(ReadTifStack(fileIn));
+% pixelSize = 108; % nm
+% 
+% imSize0 = size(img0);
+% exSize = [10, 10, 10];
+% imSize = imSize0 - exSize * 2;
+% 
+% pps = 5; % projected pixel size of 15nm
+% % typical parameters for resolution estimate
+% Nr = 50;
+% Ng = 10;
+% r = linspace(0,1,Nr);
+% GPU = 1;
+% 
+% Sz = imSize(3);
+% kcMaxs = zeros(1, Sz);
+% apSize =20;
+% 
+% dNum = (imgNumEnd - imgNumStart + 1);
+% resValues = zeros(dNum, Sz);
+% tStart = tic;
+% for i = imgNumStart:imgNumEnd
+%     cTime1 = toc(tStart);
+%     disp(['Image i #: ', num2str(i)]);
+%     fileImgSample = [fileFolderIn, fileNameBase, num2str(i), '.tif'];
+%     img0 = double(ReadTifStack(fileImgSample));
+%     img = alignsize3d(img0, imSize);
+%     for j = 1:Sz
+%         % % apodize image edges with a cosine function
+%         imSlice = apodImRect(img(:,:,j), apSize);
+%         % % compute resolution
+% 
+%         figID = 100;
+%         if GPU
+%             g = gpuDevice(1);
+%             [kcMax,A0] = getDcorr(gpuArray(imSlice),r,Ng,figID);
+%         else
+%             [kcMax,A0] = getDcorr(imSlice,r,Ng,figID);
+%         end
+%         disp(['   kcMax : ',num2str(kcMax,3),', A0 : ',num2str(A0,3)])
+%         kcMaxs(j) = kcMax;
+%         close all;
+%     end
+%     ress = pixelSize * 2./kcMaxs;
+% 
+%     resValues(i, :) =ress;
+%     cTime2 = toc(tStart);
+%     disp(['... time cost: ', num2str(cTime2-cTime1)]);
+% end
+% resMean = mean(resValues,2);
+% resSD = std(resValues',1)';
+% clear img0 img
+% csvwrite([fileOut, '.csv'], resValues);
+% csvwrite([fileOut, '_mean_SD.csv'], [resMean resSD]);
+% save([fileOut, '.mat']);
+% cTime = toc(tStart);
+% disp(['Processing completed!!! Total time cost:', num2str(cTime), ' s']);
